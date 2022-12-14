9c88a2de38c8cb45cebfdf36416d3e12df0ed8ca
mario schmoker <marioschmoker@tdf.edu.ar>
2022-12-13 23:23:01 -0300

    funcionalidad terminada

--- index.js
+++ index.js
@@ -1,64 +1,128 @@
+const state = {
+    users: null,
+    setUsers(users) {
+        this.users = users;
+    },
+}
+
 let inputName = document.getElementById('name');
 let inputEmail = document.getElementById('email');
 let inputId = document.getElementById('id');
+let form = document.querySelector('form#create-user');
 let btn = document.querySelector('.btn');
 
 btn.addEventListener('click', addUser);
+form.addEventListener('submit', updateUser);
 
 
 function addUser() {
-
-    let tr = document.createElement('tr');
-    let tdName = document.createElement('td');
-    let tdEmail = document.createElement('td');
-    let tdId = document.createElement('td');
+    const tr = document.createElement('tr');
+    const tdName = document.createElement('td');
+    const tdEmail = document.createElement('td');
+    const tdId = document.createElement('td');
 
     tdName.innerText = inputName.value;
     tdEmail.innerText = inputEmail.value;
     tdId.innerText = inputId.value;
 
-    let tbody = document.getElementById('tab');
+    const tbody = document.getElementById('tab');
     tbody.appendChild(tr);
     tr.appendChild(tdName);
     tr.appendChild(tdEmail);
     tr.appendChild(tdId);
 
+}
+
+function loadMForm(userId) {
+    const user = state.users.find(user => user.id == userId);
+
+    inputName.value = user.name;
+    inputEmail.value = user.email;
+    inputId.value = userId;
 
 }
 
+function updateUser(event) {
+    event.preventDefault();
 
+    const name = form.elements.name.value;
+    const email = form.elements.email.value;
+    const id = form.elements.id.value;
 
-fetch('https://6393e57e11ed187986bf9667.mockapi.io/api/curso/employees')
-    .then(response => response.json())
-    .then(data => {
+    const modifiedUser = {
+        name,
+        email,
+        id
+    }
+    const users = state.users;
 
-        for (let index = 0; index < data.length; index++) {
-            const element = data[index];
-            console.log(`Nombre: ${data[index].name} Email: ${data[index].email} Id: ${data[index].id}`)
-
-
-            inputName.value = element.name;
-            inputEmail.value = element.email;
-            inputId.value = element.id;
-
-            let tr = document.createElement('tr');
-            let tdName = document.createElement('td');
-            let tdEmail = document.createElement('td');
-            let tdId = document.createElement('td');
-
-            let tbody = document.getElementById('tab');
-            tbody.appendChild(tr);
-            tr.appendChild(tdName);
-            tr.appendChild(tdEmail);
-            tr.appendChild(tdId);
-
-            tdName.innerText = inputName.value;
-            tdEmail.innerText = inputEmail.value;
-            tdId.innerText = inputId.value;
-        
+    const newUsers = users.map(user => {
+        if (user.id === id) {
+            return modifiedUser;
         }
 
+        return user;
     })
 
+    state.setUsers(newUsers);
 
-    .catch(err => console.log('Hubo un problema con la petición Fetch:' + err.message))
+    renderUsers();
+}
+
+function deleteUser(userId) {
+    console.log('HOLA');
+    const users = state.users;
+
+    const index = users.findIndex(user => user.id == userId);
+
+    users.splice(index, 1);
+
+    renderUsers();
+}
+
+function renderUsers() {
+    const users = state.users;
+
+    const tbody = document.getElementById('tab');
+    tbody.innerHTML = `<tbody id="tab"></tbody>`;
+
+    for (let user of users) {
+        const tr = document.createElement('tr');
+        const tdName = document.createElement('td');
+        const tdEmail = document.createElement('td');
+        const tdId = document.createElement('td');
+        const Mbutton = document.createElement('button');
+        const Dbutton = document.createElement('button');
+
+        tbody.appendChild(tr);
+        tr.appendChild(tdName);
+        tr.appendChild(tdEmail);
+        tr.appendChild(tdId);
+        tr.append(Mbutton);
+        tr.append(Dbutton);
+
+        tdName.innerText = user.name;
+        tdEmail.innerText = user.email;
+        tdId.innerText = user.id;
+
+        Mbutton.innerHTML = `<button class="btn btn-warning" onclick="loadMForm(${user.id})">Modificar</button>`
+        Dbutton.innerHTML = `<button class="btn btn-danger" onclick="deleteUser(${user.id})">Eliminar</button>`
+    }
+
+}
+
+function setUsers() {
+    fetch('https://6393e57e11ed187986bf9667.mockapi.io/api/curso/employees')
+        .then(response => response.json())
+        .then(users => {
+            state.setUsers(users);
+
+            renderUsers();
+        })
+        .catch(err => console.log('Hubo un problema con la petición Fetch:' + err.message))
+}
+
+(function getUsers() {
+    setUsers();
+    renderUsers();
+})();
